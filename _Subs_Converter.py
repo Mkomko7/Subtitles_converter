0d17aa6d5f9791da8a115c2228635f2887bfd6d5
Mkomko7 <78308585+Mkomko7@users.noreply.github.com>
2021-11-26 21:37:48 +0100

    General code refactor - code split into functions

--- _Subs_Converter.py
+++ _Subs_Converter.py
@@ -1,34 +1,57 @@
-import pysubs2, os, traceback
+import pysubs2
+import os
+import traceback
 
-def list_files(in_dir):
-    final_list = []
-    files_list = os.listdir(in_dir)
-    for file in files_list:
-        if file.endswith('.ass'):
-            final_list.append(file)
 
-    return final_list
+class Processor:
+    def __init__(self):
+        self.in_folder = os.path.join(os.getcwd(), "in_subs")
+        self.in_sub_files = Processor.list_files(self.in_folder)
+
+    @staticmethod
+    def list_files(in_dir):
+        final_list = []
+        files_list = os.listdir(in_dir)
+        for file in files_list:
+            if file.endswith('.ass'):
+                final_list.append(file)
+
+        return final_list
+
+    def get_paths(self, in_file):
+        subs_file = os.path.join(self.in_folder, in_file)
+        out_file_path = os.path.join(os.getcwd(), "NEW_{}".format(in_file))
+        return subs_file, out_file_path
+
+    @staticmethod
+    def load_subs(in_file):
+        subs = pysubs2.load(in_file, encoding="utf-8")
+        return subs
+
+    @staticmethod
+    def process_subs(subs, shift_val, out_file):
+        subs.shift(s=shift_val)
+        subs.save(out_file)
+
 
 def main():
     try:
-        shift_val = float(input("=== Podaj wartosc przesuniecia w sekundach np 2.5\n"))
+        shift_val = float(input("=== Podaj wartosc przesuniecia w sekundach np 2.5 ===\n"))
         print("Konwertuje")
-        
-        in_folder = "in_subs"
-        in_sub_files = list_files(os.path.join(os.getcwd(), in_folder))
 
-        for in_file in in_sub_files:
+        prc = Processor()
 
-            in_sub_file = os.path.join(os.getcwd(), in_folder, in_file)
-            out_file = os.path.join(os.getcwd(), "NEW_{}".format(in_file))
-            subs = pysubs2.load(in_sub_file, encoding="utf-8")
-            subs.shift(s=shift_val)
-            subs.save(out_file)
+        for in_file in prc.in_sub_files:
+            in_sub_file, out_file_path = prc.get_paths(in_file)
+            subs = prc.load_subs(in_sub_file)
+            prc.process_subs(subs, shift_val, out_file_path)
+
         input("===Koniec. Nacisnij ENTER aby zamknac. ===")
+
     except Exception:
-        error = traceback.print_exc()
-        print("!!! Nie dziala !!!")
-        input("Nacisnij ENTER aby zamknac")
-    
+        # error = traceback.print_exc()
+        input("!!! Nie dziala !!!\nNacisnij ENTER aby zamknac")
+
+
 if __name__ == "__main__":
     main()
