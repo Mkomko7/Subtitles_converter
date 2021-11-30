461debe6f498ca1f5dc122fa1c06ea1b26e5b9ad
Mkomko7 <78308585+Mkomko7@users.noreply.github.com>
2021-11-30 21:47:02 +0100

    Code refactor for unit tests Unit tests added

---
+++ Tests/Unit_tests.py
@@ -0,0 +1,42 @@
+import unittest
+from _Subs_Converter import Processor
+
+
+class MyTestCase(unittest.TestCase):
+
+    # @classmethod
+    # def setUpClass(cls):
+    #     Processor()
+
+    def test_check_input_int(self):
+        result = Processor.check_input("5")
+        self.assertEqual(5, result)  # add assertion here
+
+    def test_check_input_float(self):
+        result = Processor.check_input("5.4")
+        self.assertEqual(5.4, result)  # add assertion here
+
+    def test_check_input_str_fail(self):
+        with self.assertRaises(ValueError):
+            Processor.check_input("asdf")
+
+    def test_check_input_empty(self):
+        with self.assertRaises(ValueError):
+            Processor.check_input("")
+
+    def test_list_sub_files_match(self):
+        files_list = ["subs.txt"]
+        extensions = (".txt", ".ass")
+        out_list = Processor.list_sub_files(files_list, extensions)
+        self.assertListEqual(files_list, out_list)
+
+    @unittest.expectedFailure
+    def test_list_sub_files_no_match(self):
+        files_list = ["subs.shp"]
+        extensions = (".txt", ".ass")
+        out_list = Processor.list_sub_files(files_list, extensions)
+        self.assertListEqual(files_list, out_list)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- _Subs_Converter.py
+++ _Subs_Converter.py
@@ -1,19 +1,35 @@
 import pysubs2
 import os
-import traceback
 
 
 class Processor:
     def __init__(self):
+        extensions_tuple = (".ass", ".txt")
         self.in_folder = os.path.join(os.getcwd(), "in_subs")
-        self.in_sub_files = Processor.list_files(self.in_folder)
+        in_files = Processor.list_files(self.in_folder)
+        self.in_sub_files = Processor.list_sub_files(in_files, extensions_tuple)
+
+    @staticmethod
+    def check_input(value):
+        shift_val = float(value)
+        return shift_val
+
+    @staticmethod
+    def get_input():
+        in_val = input("=== Podaj wartosc przesuniecia w sekundach np 2.5 ===\n")
+        # print("Konwertuje")
+        return in_val
 
     @staticmethod
     def list_files(in_dir):
-        final_list = []
         files_list = os.listdir(in_dir)
+        return files_list
+
+    @staticmethod
+    def list_sub_files(files_list, extensions):
+        final_list = []
         for file in files_list:
-            if file.endswith('.ass'):
+            if file.endswith(extensions):
                 final_list.append(file)
 
         return final_list
@@ -36,10 +52,9 @@ class Processor:
 
 def main():
     try:
-        shift_val = float(input("=== Podaj wartosc przesuniecia w sekundach np 2.5 ===\n"))
-        print("Konwertuje")
-
         prc = Processor()
+        shift_in = prc.get_input()
+        shift_val = prc.check_input(shift_in)
 
         for in_file in prc.in_sub_files:
             in_sub_file, out_file_path = prc.get_paths(in_file)
